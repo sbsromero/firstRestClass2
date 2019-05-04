df897354b361bc8e593b6b2fea3dab8cc5afb3c8
microservicios <you@example.com>
2019-05-04 09:33:09 -0500

    relaciones en la base de datos h2-console

--- src/main/java/com/multiplication/rest/multiplication/domain/MultiplicationResultAttempt.java
+++ src/main/java/com/multiplication/rest/multiplication/domain/MultiplicationResultAttempt.java
@@ -1,6 +1,13 @@
 package com.multiplication.rest.multiplication.domain;
 
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -10,17 +17,30 @@ import lombok.ToString;
 @Getter
 @ToString
 @EqualsAndHashCode
+@Entity
 public class MultiplicationResultAttempt {
-
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	@JoinColumn(name="USER_ID")
 	private final User user;
+	
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	@JoinColumn(name="MULTIPLICATION_ID")
     private final Multiplication multiplication;
+	
     private final int resultAttempt;
+    private final boolean correct;
 
     // Empty constructor for JSON (de)serialization
     MultiplicationResultAttempt() {
         user = null;
         multiplication = null;
         resultAttempt = -1;
+        correct = false;
     }
 
 }
